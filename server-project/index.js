03ef6326ec88620f119deefb02a5accbdce266da
Jassy0205 <a.z23042016a@gmail.com>
2024-04-07 01:00:36 -0500

    primeros avances

--- server-project/index.js
+++ server-project/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userRouter = require("./routes/user");
+const authRouter = require('./routes/auth');
 const passport = require('passport');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/', authRouter);
 app.use("/api/v1/users", userRouter);
 
 app.listen(3000, () => {
